808cad1a32a58140ddf23a44dc88f828454b50c0
ritz31 <riturajphukan5@gmail.com>
2021-10-07 19:06:37 +0530

    ran tsc

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,6 +1,2 @@
-import HSet from "./HSet";
-declare const _default: {
-    HSet: typeof HSet;
-};
-export default _default;
+export { default as HSet } from "./HSet";
 //# sourceMappingURL=index.d.ts.map
